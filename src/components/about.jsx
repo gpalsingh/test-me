54e7d226d770fac7c07cdc2d03827b0cea2466da
Gurkirpal <gurkirpal204@gmail.com>
2019-02-03 20:28:27 +0530

    Add about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,7 +4,14 @@ export class About extends Component {
   render() {
     return(
       <div>
-        TODO
+        <h1>About</h1>
+        <p>Simple problems involve only few values. To create new problems all you have to do
+          is change those values. This app does that work for you so that practice to your heart's content.
+        </p>
+        <p>
+          Created by: <a href="https://twitter.com/gurpalsingh204">@gurpalsingh204</a><br />
+          Github: <a href="https://github.com/gpalsingh/test-me">https://github.com/gpalsingh/test-me</a>
+        </p>
       </div>
     );
   }
